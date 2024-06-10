b3f94ba01127e0d8561169e332fc38b7ec2a1fca
CTCycle <thomas.virdis89@gmail.com>
2024-06-10 09:44:14 +0200

    changed name of data folder

--- ADSORFIT/commons/pathfinder.py
+++ ADSORFIT/commons/pathfinder.py
@@ -1,5 +1,5 @@
 from os.path import join, dirname, abspath 
 
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
-DATA_PATH = join(PROJECT_DIR, 'data')
+DATA_PATH = join(PROJECT_DIR, 'resources')
 BEST_FIT_PATH = join(DATA_PATH, 'best fit')
