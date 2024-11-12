0525aef28d50eea6c3bbc7e2296f1a9fa23aac8c
CTCycle <thomas.virdis89@gmail.com>
2024-11-12 08:47:48 +0100

    built models configuration tab

--- ADSORFIT/commons/utils/app/widgets.py
+++ ADSORFIT/commons/utils/app/widgets.py
@@ -9,14 +9,14 @@ from ADSORFIT.commons.logger import logger
 ###############################################################################
 class ModelsConfigurationWidgets:
 
-    def __init__(self):
+    def __init__(self):        
         self.model_states = {}
 
     #--------------------------------------------------------------------------
     def model_configurations(self, configurations):
-
+        
         for model_name, params in configurations['MODELS'].items():
-            # Create a list to store parameter fields for enabling/disabling
+            # Create a list to store parameter fields
             param_fields = []
 
             with ui.grid().style('grid-template-columns: 1fr 2fr 3fr'):
@@ -24,11 +24,10 @@ class ModelsConfigurationWidgets:
                 with ui.column():
                     model_state = ui.switch(text=f"Select {model_name}", value=False)
 
-                    # Register the state and fields to enable updates later
+                    # Register the state and fields for future use if needed
                     self.model_states[model_name] = (model_state, param_fields)
 
-                    # Attach the event handler, binding model_name
-                    model_state.on(partial(self.update_selection, model_name=model_name))
+                    # No need to attach the event handler since we won't enable/disable fields
 
                 # Right column: Parameters organized into three sub-columns
                 with ui.column():
@@ -37,27 +36,36 @@ class ModelsConfigurationWidgets:
                         with ui.column():
                             ui.label("Min")
                             for param, value in params["min"].items():
-                                field = ui.number(label=f"{param} (Min)", value=value, disabled=True)
+                                field = ui.number(f"{param}", value=value)
                                 param_fields.append(field)
 
                         # Max values column
                         with ui.column():
                             ui.label("Max")
                             for param, value in params["max"].items():
-                                field = ui.number(label=f"{param} (Max)", value=value, disabled=True)
+                                field = ui.number(f"{param}", value=value)
                                 param_fields.append(field)
 
                         # Initial values column
                         with ui.column():
                             ui.label("Initial")
                             for param, value in params["initial"].items():
-                                field = ui.number(label=f"{param} (Initial)", value=value, disabled=True)
+                                field = ui.number(f"{param}", value=value)
                                 param_fields.append(field)
 
     #--------------------------------------------------------------------------
-    def update_selection(self, model_name, e):
+    # The update_selection method is no longer needed and has been removed
+
+
+    #--------------------------------------------------------------------------
+    def update_selection(self, model_name, event):
         model_state, param_fields = self.model_states[model_name]
-        disabled = not e.value
+
+        # Determine the enabled/disabled state based on the switch value
+        disabled = not model_state.value
         for field in param_fields:
-            field.disabled = disabled
+            if disabled:
+                field.props('disable')
+            else:
+                field.props(remove='disable')
             field.update()
